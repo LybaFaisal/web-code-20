297cf410d9ec437fad5202c657f7feac33e1ff6b
Lyba <faisallyba06@gmail.com>
2024-01-08 20:20:43 +0500

    Temp

--- file-5.js
+++ file-5.js
@@ -14,3 +14,31 @@ else if(number==0)
 else{
     document.write("Number is less than zero");
 }
+
+
+let temp=30;
+
+
+if(temp<0)
+{
+    document.write("It's freezing!");
+}
+else if(temp>=0 && temp<10)
+{
+    document.write("Very Cold temperature");
+}
+else if(temp>=10 && temp<20)
+{
+    document.write("Cold temperature");
+}
+else if(temp>=20 && temp<30)
+{
+    document.write("Mid Temperature");
+}
+else if(temp>=30 && temp<40)
+{
+    document.write("Warm temperature");
+}
+else{
+    document.write("Hot temperature");
+}
