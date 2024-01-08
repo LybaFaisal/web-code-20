ef9593f35a4e1d8face223021cb03ad6566d378f
Lyba <faisallyba06@gmail.com>
2024-01-08 20:12:21 +0500

    Multiple Conditions

---
+++ file-5.js
@@ -0,0 +1,16 @@
+//multiple conditions
+
+let number;
+number=10;
+
+if(number>0)
+{
+    document.write("Number is greater than 0");
+}
+else if(number==0)
+{
+    document.write("Number is zero");
+}
+else{
+    document.write("Number is less than zero");
+}
